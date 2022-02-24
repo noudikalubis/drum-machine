3fa0f01553e6776aad70099e1ca7161b21fca75d
Noudika Lubis <noudikalubis@gmail.com>
2022-02-24 16:34:23 +0700

    add files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,111 @@
-import React from 'react';
+import React, { useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+const drumSound = [
+  {
+    keyCode: 81,
+    keyTrigger: 'Q',
+    id: 'Heater-1',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+  },
+  {
+    keyCode: 87,
+    keyTrigger: 'W',
+    id: 'Heater-2',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
+  },
+  {
+    keyCode: 69,
+    keyTrigger: 'E',
+    id: 'Heater-3',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
+  },
+  {
+    keyCode: 65,
+    keyTrigger: 'A',
+    id: 'Heater-4',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
+  },
+  {
+    keyCode: 83,
+    keyTrigger: 'S',
+    id: 'Clap',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
+  },
+  {
+    keyCode: 68,
+    keyTrigger: 'D',
+    id: 'Open-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
+  },
+  {
+    keyCode: 90,
+    keyTrigger: 'Z',
+    id: "Kick-n'-Hat",
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
+  },
+  {
+    keyCode: 88,
+    keyTrigger: 'X',
+    id: 'Kick',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
+  },
+  {
+    keyCode: 67,
+    keyTrigger: 'C',
+    id: 'Closed-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
+  }
+];
+
+const KeyboardKey = ({ play, sound: { id, keyTrigger, url, keyCode } }) => {
+
+  const handleKeydown = (e) => {
+    if (e.keyCode === keyCode) {
+      play(keyTrigger, id)
+    }
+  }
+
+  useEffect( () => {
+    document.addEventListener("keydown", handleKeydown)
+  }, []);
+
+  return (
+    <button className="drum-pad" id={keyTrigger} onClick={() => play(keyTrigger, id)}>
+      <audio className = "clip" id={keyTrigger} src = {url} />
+      {keyTrigger}
+    </button>    
+  )
+}
+
+const Keyboard = ({ play }) => {
+    return drumSound.map((sound) => <KeyboardKey play={play} sound={sound} />)
+}
+
+const App = () => {
+  
+  const [soundName, setSoundName] = React.useState("Click or Press a Key");
+
+  const play = (keyTrigger, id) => {
+    setSoundName(id)
+    const audio = document.getElementById(keyTrigger);
+    audio.currentTime = 0;
+    audio.play()
+  }
+  
+  return (
+    <div id = "drum-machine">
+      <div id="outer-drum">
+      <Keyboard play={play} />
+      </div>
+      <div id = "display">
+        <p>{soundName}</p>
+      </div> 
+    </div>
+  )
+} 
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +114,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
